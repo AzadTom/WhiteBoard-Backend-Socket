21f9f7ed595bd3fe86955a41ab565301a46609b4
azadtom <kumarazad2917@gmail.com>
2024-04-01 11:33:03 +0530

    version-1

---
+++ index.js
@@ -0,0 +1,63 @@
+import express from 'express';
+import http from 'http';
+
+const app = express();
+const server = http.createServer(app);
+
+import { Server } from 'socket.io';
+
+const io = new Server(server,{
+    cors:{
+        origin:"*"
+    }
+})
+
+io.on('connection',(socket)=>{
+
+    socket.on("client-ready",()=>{
+
+        console.log("connected!");
+        socket.broadcast.emit("server-ready");
+
+    });
+
+
+    socket.on('rectangle',(rect)=>{
+
+         socket.broadcast.emit('onrectangle',rect);  
+  
+    });
+
+
+    socket.on('circle',(circle)=>{
+
+        socket.broadcast.emit('oncircle',circle);  
+ 
+   });
+
+
+   socket.on('arrow',(arrow)=>{
+
+    socket.broadcast.emit('onarrow',arrow);  
+
+});
+
+
+
+socket.on('scribble',(scribble)=>{
+
+    socket.broadcast.emit('onscribble',scribble);  
+
+});
+    
+
+})
+
+
+server.listen(3001,()=>{
+
+    console.log("server is listening on port 3001");
+})
+
+
+
