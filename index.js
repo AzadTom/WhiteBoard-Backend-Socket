2aaba949d4f6c5a6e417418cf5c613ff9916309e
azadtom <kumarazad2917@gmail.com>
2024-04-01 11:51:36 +0530

    version-2

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import http from 'http';
+import { Server } from 'socket.io';
+import  cors from 'cors';
 
 const app = express();
 const server = http.createServer(app);
 
-import { Server } from 'socket.io';
+
 
 const io = new Server(server,{
     cors:{
