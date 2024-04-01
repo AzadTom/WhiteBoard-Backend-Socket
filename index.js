c611b01675e361c422b9618e734e9a615fce303d
azadtom <kumarazad2917@gmail.com>
2024-04-01 11:58:39 +0530

    version-2

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
-import  cors from 'cors';
 
 const app = express();
 const server = http.createServer(app);
@@ -10,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
     cors:{
-        origin:"*"
+        origin:"https://white-board-front-end.vercel.app/"
     }
 })
 
